75cb5a1507cff2f763b4319b367847ba3ad6dcf7
Davaadorj-B-ETT <davaadorjboldbaatar1010@gmail.com>
2026-02-09 19:15:29 +0800

    Lec2

--- myapp/src/app/page.tsx
+++ myapp/src/app/page.tsx
@@ -1,9 +1,20 @@
-import Image from "next/image";
+"use client"; // Client-side rendering идэвхжүүлж байна
 
-export default function Home() {
+import { useState } from "react";
+
+export default function Counter() {
+  // count = утга, setCount = шинэчлэх функц
+  const [count, setCount] = useState(0);
+
   return (
-    <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      Hello World
+    <div className="p-4">
+      <p>Тоолуур: {count}</p>
+      <button
+        className="px-4 py-2 bg-green-500 text-white rounded"
+        onClick={() => setCount(count + 1)} // Дарсан үед 1-ээр нэмнэ
+      >
+        Нэмэх
+      </button>
     </div>
   );
 }
