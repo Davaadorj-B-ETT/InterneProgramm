1e5c42e03b56c44485ca891285f7842b4900bfad
Davaadorj-B-ETT <davaadorjboldbaatar1010@gmail.com>
2026-02-09 19:23:33 +0800

    Week02_lab3

--- myapp/src/app/About/page.tsx
+++ myapp/src/app/About/page.tsx
@@ -1,8 +1,9 @@
+import styles from "./page.module.css"
 export default function About() {
   return (
     <div className="p-8">
-      <h1 className="text-3xl font-bold text-blue-600">Миний тухай</h1>
-      <p className="mt-4 text-gray-700">
+      <h1 className={styles.title}>Миний тухай</h1>
+      <p className={styles.description}>
         Би Next.js ашиглаж веб хөгжүүлэлт сурч байна.
       </p>
       <p className="mt-2">Миний хобби: унших, аялах, программчлал.</p>
